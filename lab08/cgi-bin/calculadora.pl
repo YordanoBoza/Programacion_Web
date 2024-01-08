02dedb9766b17698867919a1f0b1d644e176ce55
Yordano Boza Portilla <ybozap@unsa.edu.pe>
2024-01-08 01:13:30 -0500

    Creando otra variable para concatenar caracteres, ademas se hizo el manejo de botones especiales como 'AC' que elimina el contenido que se encuentra en pantalla de la calculadora, entre otros botones

--- lab08/cgi-bin/calculadora.pl
+++ lab08/cgi-bin/calculadora.pl
@@ -6,4 +6,28 @@ use CGI;
 my $cgi = CGI->new;
 my $caracter = $cgi->param('caracter');
 my $expresion_anterior = $cgi->param('expresion');
+my $expresion_actualizada;
 
+# Manejar casos especiales
+if ($caracter eq 'AC') {
+    $expresion_actualizada = '';
+}
+elsif ($caracter eq '.') {
+    if(substr($expresion_anterior,-1)=~ /\d/){
+        $expresion_actualizada = $expresion_anterior . $caracter;
+    }else{
+        $expresion_actualizada = $expresion_anterior;
+    }
+}
+elsif ($caracter eq '%') {
+    $expresion_actualizada = $expresion_anterior / 100;
+}elsif($caracter =~ /[+\-\/]/){
+    if(substr($expresion_anterior,-1)=~ /\d|\)/ ){
+        $expresion_actualizada = $expresion_anterior . $caracter;
+    }else{
+        $expresion_actualizada = $expresion_anterior;
+    }
+}
+else {
+    $expresion_actualizada = $expresion_anterior . $caracter;
+}
