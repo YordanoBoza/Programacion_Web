1d6115b22f01c54ff5387ec2b7a463fabfd29469
Yordano Boza Portilla <ybozap@unsa.edu.pe>
2024-01-09 23:02:52 -0500

    Se procesa un archivo CSV donde se encuentra la informacion a buscar, se crea una variable que contendra el encabezado del archivo, esto nos servira para facilitar la busqueda; despues se itera sobre cada fila y los datos encontrados se almacenan en un array

--- lab09/cgi-bin/consulta.pl
+++ lab09/cgi-bin/consulta.pl
@@ -9,3 +9,33 @@ my $nombre_universidad = uc($cgi->param('nombre_universidad')) || '';
 my $periodo_licenciamiento = uc($cgi->param('periodo_licenciamiento')) || '';
 my $departamento_local = uc($cgi->param('departamento_local')) || '';
 my $denominacion_programa = uc($cgi->param('denominacion_programa')) || '';
+
+print $cgi->header(type   => 'text/html',charset => 'latin-1');
+my $csv_file = 'Programas de Universidades.csv';
+
+my $csv = Text::CSV->new({ binary => 1, auto_diag => 1, sep_char => '|'});
+#abrir el archivo csv
+open my $fh, '<:encoding(latin-1)', $csv_file or die "No se puede abrir archivo CSV: $csv_file";
+
+my $header = $csv->getline($fh);
+
+
+my @resultados;
+while (my $fila = $csv->getline($fh)) {
+    my %datos;
+    @datos{@$header} = @$fila;
+    if (
+        $datos{'NOMBRE'} eq $nombre_universidad ||
+        $datos{'PERIODO_LICENCIAMIENTO'} eq $periodo_licenciamiento ||
+        $datos{'DEPARTAMENTO_LOCAL'} eq $departamento_local ||
+        $datos{'DENOMINACION_PROGRAMA'} eq $denominacion_programa
+    ) {
+        if($datos{'NOMBRE'} ne ''){
+            push @resultados, \%datos;
+        }
+
+    }
+}
+
+# Cerrar el archivo CSV
+close $fh;
